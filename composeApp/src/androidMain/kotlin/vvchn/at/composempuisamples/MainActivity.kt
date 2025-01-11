5cc3b9beb06b8b1a1749cf37d5e4e539e811fb26
vvchn <vl.soldatov4@gmail.com>
2025-01-11 06:00:30 +0300

    Enable EdgeToEdge for Android target

--- composeApp/src/androidMain/kotlin/vvchn/at/composempuisamples/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/vvchn/at/composempuisamples/MainActivity.kt
@@ -3,12 +3,14 @@ package vvchn.at.composempuisamples
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.arkivanov.decompose.defaultComponentContext
 import vvchn.at.composempuisamples.navigation.RootComponent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         val appNavComponent = RootComponent(defaultComponentContext())
         setContent {
             App(appNavComponent)
