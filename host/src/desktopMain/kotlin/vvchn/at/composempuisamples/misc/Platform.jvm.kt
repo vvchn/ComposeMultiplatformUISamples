33dc6d8fcde824592cd4fdac3aa3a437e1a13526
vvchn <vl.soldatov4@gmail.com>
2025-01-27 23:33:03 +0300

    Add SDK info for JVM targets

--- host/src/desktopMain/kotlin/vvchn/at/composempuisamples/misc/Platform.jvm.kt
+++ host/src/desktopMain/kotlin/vvchn/at/composempuisamples/misc/Platform.jvm.kt
@@ -5,10 +5,18 @@ import androidx.compose.runtime.Stable
 
 @Immutable
 data class JVMEnvironment(
-    override val systemInfo: String = "${System.getProperty("os.name")} ${System.getProperty("os.arch")}",
-    override val sdkInfo: String? = null,
+    override val systemInfo: String = getSystemInfo(),
+    override val sdkInfo: String? = "Java ${System.getProperty("java.version")} ${System.getProperty("java.vendor")}",
     override val appVersion: String = "2025.0.1"
 ) : Environment
 
 @Stable
 actual fun getEnvironment(): Environment = JVMEnvironment()
+
+@Stable
+private fun getSystemInfo(): String {
+    val osName = System.getProperty("os.name")
+    val version = if (osName.contains("win", true)) "" else "${System.getProperty("os.version")} "
+    val arch = System.getProperty("os.arch")
+    return "$osName $version$arch"
+}
