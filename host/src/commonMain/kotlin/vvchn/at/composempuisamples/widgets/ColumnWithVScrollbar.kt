5c8741f65a8ce508e3e8fba2c4b5e57d33bfaa87
vvchn <vl.soldatov4@gmail.com>
2025-02-07 06:55:47 +0300

    Implement scroll thumb drag behavior

--- host/src/commonMain/kotlin/vvchn/at/composempuisamples/widgets/ColumnWithVScrollbar.kt
+++ host/src/commonMain/kotlin/vvchn/at/composempuisamples/widgets/ColumnWithVScrollbar.kt
@@ -3,6 +3,8 @@ package vvchn.at.composempuisamples.widgets
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.TweenSpec
 import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.hoverable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsHoveredAsState
@@ -21,6 +23,7 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.structuralEqualityPolicy
 import androidx.compose.ui.Alignment
@@ -28,10 +31,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.MeasurePolicy
 import androidx.compose.ui.unit.Dp
+import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import vvchn.at.composempuisamples.misc.safeRoundToInt
 import vvchn.at.composempuisamples.theme.HostTheme
 
@@ -97,6 +105,7 @@ private fun ColumnWithVScrollbarLayout(
     }
 }
 
+// TODO: Clickable Scroll Bar
 @Composable
 private fun DefaultVScrollBar(
     modifier: Modifier = Modifier,
@@ -110,19 +119,40 @@ private fun DefaultVScrollBar(
             scrollState.isScrollInProgress
         }
     }
+
     val isHovered by interactionSource.collectIsHoveredAsState()
 
-    val measurePolicy = remember { vScrollMeasurePolicy(scrollState) }
+    val dragScope = rememberCoroutineScope()
+
+    val dragMutex by remember { mutableStateOf(Mutex()) }
+
+    var isScrollThumbBeingDragged by remember { mutableStateOf(false) }
+
+    var scrollLayoutHeight by remember { mutableStateOf(0) }
 
     val radius = HostTheme.hostDimens.scrollBarCornerRadius
 
     val color by animateColorAsState(
-        targetValue = if (isScrollVisible) HostTheme.hostColors.scrollBoxColor else Color.Transparent,
-        animationSpec = if (isScrollVisible) TweenSpec(0) else TweenSpec(durationMillis = SCROLL_BOX_FADEOUT_DURATION)
+        targetValue = if (isScrollVisible) {
+            HostTheme.hostColors.scrollBoxColor
+        } else {
+            Color.Transparent
+        },
+        animationSpec = if (isScrollVisible) {
+            TweenSpec(0)
+        } else {
+            TweenSpec(durationMillis = SCROLL_BOX_FADEOUT_DURATION)
+        }
     )
 
-    LaunchedEffect(key1 = isScrollInProgress, key2 = isHovered) {
-        if (isScrollInProgress || isHovered) {
+    val measurePolicy = remember { vScrollMeasurePolicy({ scrollLayoutHeight = it }, scrollState) }
+
+    LaunchedEffect(
+        key1 = isScrollInProgress,
+        key2 = isHovered,
+        key3 = isScrollThumbBeingDragged
+    ) {
+        if (isScrollInProgress || isHovered || isScrollThumbBeingDragged) {
             isScrollVisible = true
         } else {
             delay(SCROLL_BOX_FADEOUT_DELAY)
@@ -135,6 +165,24 @@ private fun DefaultVScrollBar(
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .pointerInput(Unit) {
+                        detectDragGestures(
+                            onDragStart = { isScrollThumbBeingDragged = true },
+                            onDrag = { change, dragAmount ->
+                                dragScope.launch(start = CoroutineStart.UNDISPATCHED) {
+                                    dragMutex.withLock {
+                                        isScrollVisible = true
+                                        scrollState.scrollBy(
+                                            dragAmount.y * (scrollState.maxValue / scrollLayoutHeight)
+                                        )
+                                    }
+                                }
+                                change.consume()
+                            },
+                            onDragEnd = { isScrollThumbBeingDragged = false },
+                            onDragCancel = { isScrollThumbBeingDragged = false }
+                        )
+                    }
                     .drawBehind {
                         drawRoundRect(
                             cornerRadius = CornerRadius(
@@ -151,8 +199,12 @@ private fun DefaultVScrollBar(
     )
 }
 
-private fun vScrollMeasurePolicy(scrollState: ScrollState): MeasurePolicy {
+private fun vScrollMeasurePolicy(
+    onScreenHeightChanged: (Int) -> Unit,
+    scrollState: ScrollState
+): MeasurePolicy {
     return MeasurePolicy { measurable, constraints ->
+        onScreenHeightChanged(constraints.maxHeight)
         val maxHeightFloat = constraints.maxHeight.toFloat()
         val totalContentHeightFloat = scrollState.maxValue + maxHeightFloat
 
@@ -160,7 +212,7 @@ private fun vScrollMeasurePolicy(scrollState: ScrollState): MeasurePolicy {
             ((maxHeightFloat / totalContentHeightFloat) * maxHeightFloat).safeRoundToInt()
 
         val scrollbarOffset = ((scrollState.value / scrollState.maxValue.toFloat()) *
-                    (maxHeightFloat - scrollbarHeight)).safeRoundToInt()
+                (maxHeightFloat - scrollbarHeight)).safeRoundToInt()
 
         val scrollBarConstraints = constraints.copy(minHeight = scrollbarHeight)
 
